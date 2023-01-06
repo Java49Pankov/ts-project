dd25dea26c865013f59a6bf96f9c076ff778ddd4
EvgenyPankov <volvovnl123@gmail.com>
2023-01-06 10:02:59 +0200

    solution HW#30

--- app.js
+++ app.js
@@ -1,36 +1,28 @@
 "use strict";
-for (let i = 0; i < 3; i++) {
-    setTimeout(() => console.log(i));
-}
-let a = 10;
-console.log(`a = ${a}`);
-/*-----------variables----------*/
-const hello = "world";
-// hello = "ggggg"; // error: const not change
-let hi = "world";
-// hi = 10; // error: hi this type string not number;
-hi = "vasya";
-let b; // let b it's any type
-b = 10;
-b = "abs";
-/*---------------------------- */
-let c;
-//c = 10; // error: c it's type string
-c = "10";
-function getRandomNumber(min, max) {
-    return Math.floor(min + Math.random() * (max - min + 1));
-}
-//HW #30
 function shiftCipher(str, shift = 1) {
-    //TODO
-    //1.for each lower case letter (a-z) you should perform code ASCII on a given shift.
-    //2.shift should be in cycleof lower case letter.
-    //code ASCII 'z' + 2 = 'b' 
-    //examples:
-    //shiftCipher ("abz.", 3) => "dec."
-    return "";
+    str = str.toLowerCase();
+    let res = "";
+    for (let i = 0; i < str.length; i++) {
+        let digit = str.charCodeAt(i);
+        if (digit >= 97 && digit <= 122) {
+            let symb = String.fromCharCode(((digit - 97 + shift) % 26) + 97);
+            res += symb;
+        }
+    }
+    return res;
 }
-function shiftDecipfer(str, shift = -1) {
-    return "";
+console.log(shiftCipher("ab1-zFaGA123-", 3));
+function shiftDecipher(str, shift = 1) {
+    str = str.toLowerCase();
+    let res = "";
+    for (let i = 0; i < str.length; i++) {
+        let digit = str.charCodeAt(i);
+        if (digit >= 97 && digit <= 123) {
+            let symb = String.fromCharCode(((digit - 97 - shift + 26) % 26) + 97);
+            res += symb;
+        }
+    }
+    return res;
 }
+console.log(shiftDecipher("abz.", 3));
 //# sourceMappingURL=app.js.map
--- app.ts
+++ app.ts
@@ -1,44 +1,30 @@
-for (let i = 0; i < 3; i++) {
-    setTimeout(() => console.log(i));
-}
-let a = 10;
-console.log(`a = ${a}`);
-
-/*-----------variables----------*/
-
-const hello = "world";
-// hello = "ggggg"; // error: const not change
-
-let hi = "world";
-// hi = 10; // error: hi this type string not number;
-hi = "vasya";
-
-let b; // let b it's any type
-b = 10;
-b = "abs";
-
-/*---------------------------- */
-let c: string;
-//c = 10; // error: c it's type string
-c = "10";
-
-function getRandomNumber(min: number, max: number): number {
-    return Math.floor(min + Math.random() * (max - min + 1));
-}
-
-//HW #30
 
 function shiftCipher(str: string, shift: number = 1): string {
-    //TODO
-    //1.for each lower case letter (a-z) you should perform code ASCII on a given shift.
-    //2.shift should be in cycleof lower case letter.
-    //code ASCII 'z' + 2 = 'b' 
-    //examples:
-    //shiftCipher ("abz.", 3) => "dec."
-    return "";
+    str = str.toLowerCase();
+    let res: string = "";
+    for (let i = 0; i < str.length; i++) {
+        let digit = str.charCodeAt(i);
+        if (digit >= 97 && digit <= 122) {
+            let symb = String.fromCharCode(((digit - 97 + shift) % 26) + 97);
+            res += symb;
+        }
+    }
+    return res;
 }
+console.log(shiftCipher("ab1-zFaGA123-", 3));
 
-function shiftDecipfer(str: string, shift: number = -1): string {
-    return "";
+function shiftDecipher(str: string, shift: number = 1): string {
+    str = str.toLowerCase();
+    let res: string = "";
+    for (let i = 0; i < str.length; i++) {
+        let digit = str.charCodeAt(i);
+        if (digit >= 97 && digit <= 123) {
+            let symb = String.fromCharCode(((digit - 97 - shift + 26) % 26) + 97);
+            res += symb;
+        }
+    }
+    return res;
 }
+console.log(shiftDecipher("abz.", 3));
+
 
