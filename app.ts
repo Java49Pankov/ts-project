547ef0fd9ba6b7ffdeee3d3d8ea8d78df223a732
EvgenyPankov <volvovnl123@gmail.com>
2023-01-07 07:44:45 +0200

    correct uppercase

--- app.ts
+++ app.ts
@@ -1,6 +1,5 @@
 
 function shiftCipher(str: string, shift: number = 1): string {
-    str = str.toLowerCase();
     let res: string = "";
     for (let i = 0; i < str.length; i++) {
         let digit = str.charCodeAt(i);
@@ -8,18 +7,21 @@ function shiftCipher(str: string, shift: number = 1): string {
             let symb = String.fromCharCode(((digit - 97 + shift) % 26) + 97);
             res += symb;
         }
+        else {
+            let symUp = String.fromCharCode(digit);
+            res += symUp;
+        }
     }
     return res;
 }
 let inputShift = [26, 27, 1000, 5];
-["abz", "abz", "abz", "123=-ASnKb"].forEach((elem, ind) => {
+["abz", "abz", "abz", "123-ASnKb"].forEach((elem, ind) => {
     console.log(`these letters ${elem} shift to the right by ${inputShift[ind]}, Result =  ${shiftCipher(elem, inputShift[ind])}`)
 })
 
 console.log("==============================================");
 
 function shiftDecipher(str: string, shift: number = 1): string {
-    str = str.toLowerCase();
     if (shift > 26) {
         shift %= 26;
     }
@@ -29,11 +31,15 @@ function shiftDecipher(str: string, shift: number = 1): string {
         if (digit >= 97 && digit <= 123) {
             let symb = String.fromCharCode(((digit - 97 - shift + 26) % 26) + 97);
             res += symb;
+        } else {
+            let symUp = String.fromCharCode(digit);
+            res += symUp;
         }
+
     }
     return res;
 }
 let inpShift = [3, 27, 1000, 6];
-["dec", "bca", "mnl", "123=-ASnKb"].forEach((elem, ind) => {
+["dec", "bca", "mnl", "123-ASnKb"].forEach((elem, ind) => {
     console.log(`these letters ${elem} shift to the left by ${inpShift[ind]}, Result =  ${shiftDecipher(elem, inpShift[ind])}`)
 })
