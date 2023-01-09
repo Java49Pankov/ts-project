b6192e58456bc1384131d47949eea373434ef93d
EvgenyPankov <volvovnl123@gmail.com>
2023-01-09 17:50:10 +0200

    OOP & TypeScript initial syntax

---
+++ app.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const Rectangle_1 = require("./Rectangle");
+const Square_1 = require("./Square");
+const shapes = [
+    new Rectangle_1.Rectangle(3, 4),
+    new Square_1.Square(5)
+];
+const sumOfShapes = shapes.reduce((result, current) => result + current.getSquare(), 0);
+console.log(sumOfShapes);
+//# sourceMappingURL=app.js.map
---
+++ app.ts
@@ -0,0 +1,12 @@
+import { Shape } from "./ShapeInterface";
+import { Rectangle } from "./Rectangle";
+import { Square } from "./Square";
+
+const shapes: Array<Shape> = [
+    new Rectangle(3, 4),
+    new Square(5)
+];
+
+const sumOfShapes: number = shapes.reduce((result, current) =>
+    result + current.getSquare(), 0)
+    console.log(sumOfShapes)
